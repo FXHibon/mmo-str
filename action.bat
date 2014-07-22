25696e57bb88aa6a97ad13c976c218bfffd67a51
FXHibon <djxf44@gmail.com>
2014-07-22 18:59:51 +0200

    Adding bower.json

--- action.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd C:\Users\Fx\Documents\GitHub\mmo-str
-echo "================== GIT PULL =================="
-git pull
-timeout /t 15 >nul
-echo "================== npm start =================="
-npm start
