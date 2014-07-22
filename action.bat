6898aef10c17c5d9a5364fd995b57143b4f74ab9
FXHibon <djxf44@gmail.com>
2014-07-22 17:31:18 +0200

    Updating internal-signin css

--- action.bat
+++ action.bat
@@ -1,7 +1,7 @@
 @echo off
 cd C:\Users\Fx\Documents\GitHub\mmo-str
-echo "================== GIT PULL ==================" > log.txt
+echo "================== GIT PULL =================="
 git pull
 timeout /t 15 >nul
-echo "================== npm start =================="  > log.txt
+echo "================== npm start =================="
 npm start
